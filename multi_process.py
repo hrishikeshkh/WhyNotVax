4ad7be44d9ef12464ffcda66e4fc66364e59ce86
hrishikeshkh <49692394+hrishikeshkh@users.noreply.github.com>
2023-07-11 00:23:07 +0530

    torch

--- multi_process.py
+++ multi_process.py
@@ -41,3 +41,8 @@ def train_batch(batch):
 with Pool(processes=4) as pool:
     for epoch in range(10):
         pool.map(train_batch, dataloader)
+        \
+
+
+#{'unnecessary': 0.9289672544080605, 'mandatory': 0.9385390428211587, 'pharma': 0.8876574307304785, 'conspiracy': 0.9486146095717884, 'political': 0.945088161209068, 'country': 0.981360201511335, 'rushed': 0.9007556675062972, 'ingredients': 0.9561712846347608, 'side-effect': 0.8382871536523929, 'ineffective': 0.8670025188916877, 'religious': 0.9944584382871536, 'none': 0.9445843828715366}
+
