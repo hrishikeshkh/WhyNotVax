412986bd26fd635d918f6732215b3d5ab18688c9
Darshanbankapure <122448204+Darshanbankapure@users.noreply.github.com>
2023-07-14 22:31:02 +0530

    added model(testing)

--- lstm.py
+++ lstm.py
@@ -1,14 +1,47 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, Embedding
 
-# Define the model
+df = pd.read_csv('data/train_val.csv')
+print(df.head)
+
+tweets = df['tweet'].values
+labels = df['labels'].values
+
+# Tokenize the tweets
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(tweets)
+sequences = tokenizer.texts_to_sequences(tweets)
+
+# Pad sequences
+max_length = 100  # Define the maximum length of your input tweets
+padded_sequences = pad_sequences(sequences, maxlen=max_length)
+
+# Encode the labels
+num_labels = len(np.unique(labels))
+encoded_labels = to_categorical(labels)
+
+# Split the data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(padded_sequences, encoded_labels, test_size=0.2, random_state=42)
+
+# Define the LSTM model
 model = Sequential()
-model.add(Embedding(max_words, embedding_dim, input_length=max_length))
+model.add(Embedding(input_dim=len(tokenizer.word_index) + 1, output_dim=100, input_length=max_length))
 model.add(LSTM(128, activation='relu'))
-model.add(Dense(13, activation='softmax'))
+model.add(Dense(num_labels, activation='softmax'))
 
 # Compile the model
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# Print the model summary
-model.summary()
+# Train the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100, batch_size=32)
+
+# Evaluate the model
+loss, accuracy = model.evaluate(X_test, y_test)
+print(f"Test Loss: {loss:.4f}")
+print(f"Test Accuracy: {accuracy:.4f}")
