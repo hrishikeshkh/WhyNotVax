c84a4e1b3a97bfe45a81e10e356abe981859c870
hrishikeshkh <49692394+hrishikeshkh@users.noreply.github.com>
2023-07-23 01:24:52 +0530

    changed loss param

---
+++ imbalance.py
@@ -0,0 +1,14 @@
+'''bunch of grpahs to do:
+
+1. Oversampling factor vs accuracy 
+2. hyperparam tuning for new criterion
+3. this model is may not be the best for imblance use trees
+
+'''
+
+#[1,2,3,4,4,4,4,5,6,7,8]
+
+[0- 1]: 1
+[1 -2]:1
+[4 -5]: 3
+
--- lstm.py
+++ lstm.py
@@ -84,4 +84,3 @@ model.summary()
 loss, accuracy = model.evaluate(X_test, y_test)
 print(f"Test Loss: {loss:.4f}")
 print(f"Test Accuracy: {accuracy:.4f}")
-
