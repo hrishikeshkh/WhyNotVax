60e0ac527154ffdef23eb6925f91a2cb61bcea98
Darshanbankapure <122448204+Darshanbankapure@users.noreply.github.com>
2023-08-03 07:30:36 +0530

    56

--- lstm.py
+++ lstm.py
@@ -7,38 +7,13 @@ from keras.layers import Dense, Embedding, LSTM, GRU, Activation, Dropout, Globa
 from keras.utils import to_categorical
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, Embedding
-import re
+import emoji ,re
 
-df = pd.read_csv('data/train_val.csv')
+df = pd.read_csv('data/train_val_cleaned.csv')
 
-def clean(text):
-    # Remove "@" tags
-    text = re.sub(r'@\w+', '', text)
-    emoji_pattern = re.compile("["
-        u"\U0001F600-\U0001F64F"  # emoticons
-        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-        u"\U0001F680-\U0001F6FF"  # transport & map symbols
-         u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                            "]+", flags=re.UNICODE)
-    text= emoji_pattern.sub(r'', text)
-     # Remove emojis
-     #text = emoji.get_emoji_regexp().sub('', text)
-    
-    return text
-
-df['tweet'] = df['tweet'].apply(clean)
 tweets = df['tweet'].values
 labels = df['labels'].values
-def clean(text):
-    # Remove "@" tags
-    text = re.sub(r'@\w+', '', text)
-    
-    # Remove emojis
-    text = emoji.get_emoji_regexp().sub('', text)
-    
-    return text
 
-df['tweet'] = df['tweet'].apply(clean)
 # Tokenize the tweets
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(tweets)
@@ -70,20 +45,19 @@ X_train, X_test, y_train, y_test = train_test_split(padded_sequences, encoded_la
 model = Sequential()
 model.add(Embedding(input_dim=len(tokenizer.word_index) + 1, output_dim=100, input_length=max_length))
 model.add(Dropout(0.6))
-model.add(Conv1D(filters = 50, kernel_size = 20, padding='valid', activation='relu', strides=1))
+model.add(Conv1D(filters = 50, kernel_size = 20, padding='valid', activation='relu', strides=2))
 #model.add(GlobalMaxPool1D())
 model.add(LSTM(128, activation='relu'))
 model.add(Dense(num_labels, activation='softmax'))
 
 # Compile the model
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Train the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=50, batch_size=32)
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=25, batch_size=64)
 
 # Evaluate the model
 model.summary()
 loss, accuracy = model.evaluate(X_test, y_test)
 print(f"Test Loss: {loss:.4f}")
 print(f"Test Accuracy: {accuracy:.4f}")
-
