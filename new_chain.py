08c8185afb0ac909dec46e767ba80f0d2bae64af
hrishikeshkh <49692394+hrishikeshkh@users.noreply.github.com>
2023-08-14 22:26:57 +0530

    2 files

--- new_chain.py
+++ new_chain.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
 
 # Assume X_text and y_multilabel are the text data and corresponding multi-label targets
 #Get the dataframe
@@ -23,7 +24,7 @@ tfidf_vectorizer = TfidfVectorizer(ngram_range=(1, 2))
 X_train_tfidf = tfidf_vectorizer.fit_transform(X_train_text)
 X_test_tfidf = tfidf_vectorizer.transform(X_test_text)
 
-# Select the top 20k features (if available)
+# Select the top 20k features using f_classif
 feature_selector = SelectKBest(chi2, k=min(20000, X_train_tfidf.shape[1]))
 X_train_selected = feature_selector.fit_transform(X_train_tfidf, y_train)
 X_test_selected = feature_selector.transform(X_test_tfidf)
@@ -32,18 +33,35 @@ mlp_classifier = MLPClassifier(
     hidden_layer_sizes=(32, 64), # Example: Two hidden layers with 100 and 50 neurons
     activation='relu',
     solver='adam',
-    alpha=0.0001,
+    alpha=0.3,
     learning_rate='constant',
-    max_iter=20
+    max_iter=10,
 )
+
+param_grid = {
+    'base_estimator__hidden_layer_sizes': [(32,), (32, 64)],
+    'base_estimator__alpha' : [0.2, 0.3, 0.4],
+    #'base_estimator__learning_rate' : [0.0001, 0.0004],
+    'base_estimator__max_iter' : [20, 30]
+    # Add other hyperparameters as needed
+}
 
 # Create a Classifier Chain
 chain = ClassifierChain(base_estimator=mlp_classifier, order='random', random_state=42)
 
 # Fit the chain on the training data
-chain.fit(X_train_selected, y_train)
+#chain.fit(X_train_selected, y_train)
 
-# Predict on the test data
+
+grid_search = GridSearchCV(chain, param_grid, cv=5, n_jobs= -1) # cv=5 specifies 5-fold cross-validation
+
+# Fit the grid search to the data
+grid_search.fit(X_train_selected, y_train)
+
+# Get the best parameters and estimator
+best_params = grid_search.best_params_
+best_estimator = grid_search.best_estimator_
+
 y_pred = chain.predict(X_test_selected)
 
 # Now, y_pred contains the multi-label predictions
